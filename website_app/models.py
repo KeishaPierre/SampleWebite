992cdec90fad648be530d7a963d1178cbb49d059
KeishaPierre <hopeamyturner@gmail.com>
2022-04-27 10:57:35 -0400

    used html to put buy button in for loop on index

--- website_app/models.py
+++ website_app/models.py
@@ -44,7 +44,7 @@ class Products(models.Model):
     category = models.ForeignKey("Category", on_delete=models.CASCADE, blank=True)
     description = models.CharField(max_length=250, null=True, blank=False)
     product_image = models.ImageField(null=True, blank=True, upload_to='media/media/images/', default='media/images/tennisball.jpg')
-    
+    # buybutton = models.ForeignKey("Buy", on_delete=models.CASCADE, blank=True)
     def __str__(self):
         return self.name
     
